6bbeac597d584d7abba60d0db6089f08d8172e0d
Tim McIver <tim@timmciver.com>
2015-01-22 22:54:07 -0500

    Added some CSS for display of 'lifts' table and updated the content template a bit.

--- templates/part.content.php
+++ templates/part.content.php
@@ -1,12 +1,12 @@
 
 <div>
-    <h3>Lifts</h3>
+    <h1>Lifts</h1>
     <table border="1">
         <tr><th>ID</th><th>Lift Name</th></tr>
         <?php foreach($_['lifts'] as $lift) { ?>
             <tr>
-                <td><?php p($lift->getId()); ?></td>
-                <td><?php p($lift->getName()); ?></td>
+		<td><?php p($lift->getId()); ?></td>
+		<td><?php p($lift->getName()); ?></td>
             </tr>
         <?php
         }?>
